a0ed07545490bededdbf8818e558458d13710637
heyy.lol <131338681+heyy-lol@users.noreply.github.com>
2024-01-08 00:12:33 -0800

    Update GradientMaskViewModifier.swift

--- GradientMaskViewModifier.swift
+++ GradientMaskViewModifier.swift
@@ -44,7 +44,7 @@ struct GradientMaskPreview: View {
         Text("Tap to randomize")
             .gradientMask(colors: $gradientColors)
             .onTapGesture {
-                self.gradientColors = [.random, .random]
+                gradientColors = [.random, .random]
             }
     }
 }
